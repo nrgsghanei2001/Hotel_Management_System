3582559c38160fce1e56a9016e56b559eec1db30
nargesghanei2001 <nargesghanei031@gmail.com>
2023-07-10 18:03:24 +0330

    create mode reserves, reserve items, calender

--- hotel/admin.py
+++ hotel/admin.py
@@ -4,7 +4,36 @@ from .models import *
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ['room_number', 'capacity', 'availability', 'price']
+    list_display = ['room_number', 'capacity', 'price']
     list_display_links = ['room_number']
+
+
+@admin.register(calender)
+class calenderAdmin(admin.ModelAdmin):
+    list_display = ['month', 'day']
+
+
+@admin.register(reserve_item)
+class reserveItemAdmin(admin.ModelAdmin):
+    list_display = ['room', 'time']
+
+    @admin.display(description='staying time')
+    def time(self, obj):
+        return "room "+str(obj.room.room_number) + " days:"+ " , ".join(f"{s.month}/{s.day}" for s in obj.staying_time.all())
+
+
+
+@admin.register(Reserves)
+class reservesAdmin(admin.ModelAdmin):
+    list_display = ['Guest', 'Reserves']
+
+    @admin.display(description='Guest')
+    def Guest(self, obj):
+        return obj.guest.user.username
+
+    @admin.display(description='Reserves')
+    def Reserves(self, obj):
+        return " , ".join(r.item_name() for r in obj.reserve_item.all())
+
 
 
--- hotel/models.py
+++ hotel/models.py
@@ -1,11 +1,51 @@
 from django.db import models
+from accounts.models import Guest
 
 
 class Room(models.Model):
     room_number = models.IntegerField(null=False, blank=False, unique=True)
     capacity = models.IntegerField(null=False, blank=False)
-    availability = models.BooleanField(null=True, blank=True)
+    # availability = models.BooleanField(null=True, blank=True)
     price = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return str(self.room_number)
+
+
+class calender(models.Model):
+    Months = (
+      ("1", 'January'),('2', 'February'),('3', 'March'),('4', 'April'),('5',"May"), ('6', "June"), ('7', "July"),
+       ('8', "August"), ('9', "September"), ('10', "October"), ('11', "November"), ('12', "December"))
+
+    Days = (('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), 
+    ('10','10'), ('11','11'), ('12','12'), ('13','13'), ('14','14'), ('15','15'),('16','16'), ('17','17'), ('18','18'),
+     ('19','19'), ('20','20'),('21','21'), ('22','22'), ('23','23'), ('24','24'), ('25','25'),('26','26'), ('27','27'),
+     ('28','28'), ('29','29'), ('30','30'))
+
+    month = models.CharField(max_length=2, choices=Months, default=1)
+    day = models.CharField(max_length=2, choices=Days, default=1)
+
+    def __str__(self):
+        return f"{self.month}/{self.day}"
+
+
+class reserve_item(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE ,related_name='reserve_item', null=True)
+    staying_time = models.ManyToManyField(calender, related_name="reserve_item")
+
+    def item_name(self):
+        return str(self.room.room_number) +" " + " , ".join(f"{s.month}/{s.day}" for s in self.staying_time.all())
+
+    def __str__(self):
+        return str(self.room.room_number) +" " + " , ".join(f"{s.month}/{s.day}" for s in self.staying_time.all())
+
+
+class Reserves(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name="Reserves")
+    reserve_item = models.ManyToManyField(reserve_item, related_name="Reserves")
+
+    def item_name(self):
+        return self.guest.user.username  + " , ".join(r.item_name() for r in self.reserve_item.all())
+
+    def __str__(self):
+        return self.guest.user.username  + " , ".join(r.item_name() for r in self.reserve_item.all())

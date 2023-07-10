e3f100d562bc4b7ba5ac74a7930aaf6e52dc5952
nargesghanei2001 <nargesghanei031@gmail.com>
2023-07-10 14:27:57 +0330

    filter rooms by status, price, and capacity option

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('visit_rooms/', views.visit_rooms, name='visit_rooms'),
+    path('visit_rooms/capacity/', views.visit_rooms_capacity, name='visit_rooms_capacity'),
+    path('visit_rooms/price/', views.visit_rooms_price, name='visit_rooms_price'),
+    path('visit_rooms/status/', views.visit_rooms_status, name='visit_rooms_status'),
     path('service/', views.service, name='service'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
 from .models import *
 
 
@@ -6,6 +8,24 @@ def visit_rooms(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'hotel/visit_rooms.html', context)
+
+
+def visit_rooms_capacity(request):
+    rooms = Room.objects.all().order_by('capacity')
+    context = {'rooms':rooms}
+    return render(request, 'hotel/visit_rooms_capacity.html', context)
+
+
+def visit_rooms_price(request):
+    rooms = Room.objects.all().order_by('price')
+    context = {'rooms':rooms}
+    return render(request, 'hotel/visit_rooms_price.html', context)
+
+
+def visit_rooms_status(request):
+    rooms = Room.objects.filter(availability=True)
+    context = {'rooms':rooms}
+    return render(request, 'hotel/visit_rooms_status.html', context)
 
 
 def service(request):

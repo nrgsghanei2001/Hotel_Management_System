fac04f6a1e38138fcd994bfb8d514c1732f76441
ashkanzarkhah <ashkanzarkhah@gmail.com>
2023-07-13 16:34:54 +0330

    Fixing Order bugs

--- hotel/views.py
+++ hotel/views.py
@@ -19,6 +19,7 @@ def get_room_number(request):
                     return roomNumber
     return roomNumber
 
+
 def visit_rooms(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
@@ -45,16 +46,19 @@ def visit_rooms_status(request):
 
 def service(request):
     return render(request, 'hotel/service.html')
-  
+
+
 def request_installation(request):
     req = list(Installation_request.objects.filter(guest__user=request.user))
-    context = {'req' : req}
+    context = {'req': req}
     return render(request, 'hotel/request_installation.html', context)
+
 
 def manage_installation(request):
     req = Installation_request.objects.all().order_by('request_date')
-    context = {'req' : req}
+    context = {'req': req}
     return render(request, 'hotel/manage_installation.html', context)
+
 
 def create_request(request):
     if request.method == 'POST':
@@ -71,10 +75,10 @@ def create_request(request):
             for obj2 in obj.reserve_item.all():
                 for obj3 in list(obj2.staying_time.all()):
                     d1 = '{d.month}/{d.day}'.format(d=datetime.datetime.now())
-                    if(d1 == str(obj3)):
+                    if (d1 == str(obj3)):
                         roomNumber = obj2.room.room_number
                         print(roomNumber)
-        if(roomNumber == None):
+        if (roomNumber == None):
             return render(request, 'hotel/failed_request.html')
         results = None
         cost = 0
@@ -83,11 +87,12 @@ def create_request(request):
             request_date=request_date,
             guest=guest,
             results=results,
-            roomNumber = roomNumber,
-            cost= cost
+            roomNumber=roomNumber,
+            cost=cost
         )
         installation_request.save()
     return render(request, 'hotel/create_request.html')
+
 
 def ins_req_result(request):
 
@@ -95,20 +100,20 @@ def ins_req_result(request):
     objlist = []
     if request.method == "POST":
         for obj in install_requests:
-            if(str(request.POST.get('id')) == str(obj.id)):
-                objlist.append(obj)    
+            if (str(request.POST.get('id')) == str(obj.id)):
+                objlist.append(obj)
         context = {'req': objlist}
         return render(request, 'hotel/ins_req_result.html', context)
-    
+
 
 def update_result_ins(request):
     objlist = []
-    install_requests = list(Installation_request.objects.all())    
+    install_requests = list(Installation_request.objects.all())
     if request.method == "POST":
         for obj in install_requests:
             print(str(request.POST['id']))
             print(str(obj.id))
-            if(str(request.POST.get('id')) == str(obj.id)):
+            if (str(request.POST.get('id')) == str(obj.id)):
                 obj.results = request.POST['description']
                 obj.cost = request.POST['cost']
                 objlist.append(obj)
@@ -119,8 +124,9 @@ def update_result_ins(request):
 
 def request_housekeeping(request):
     req = list(Housekeeping_request.objects.filter(guest__user=request.user))
-    context = {'req' : req}
+    context = {'req': req}
     return render(request, 'hotel/request_housekeeping.html', context)
+
 
 def create_request_hk(request):
     if request.method == 'POST':
@@ -137,10 +143,10 @@ def create_request_hk(request):
             for obj2 in obj.reserve_item.all():
                 for obj3 in list(obj2.staying_time.all()):
                     d1 = '{d.month}/{d.day}'.format(d=datetime.datetime.now())
-                    if(d1 == str(obj3)):
+                    if (d1 == str(obj3)):
                         roomNumber = obj2.room.room_number
                         print(roomNumber)
-        if(roomNumber == None):
+        if (roomNumber == None):
             return render(request, 'hotel/failed_request.html')
         results = None
         cost = 0
@@ -149,41 +155,45 @@ def create_request_hk(request):
             request_date=request_date,
             guest=guest,
             results=results,
-            roomNumber = roomNumber,
-            cost= cost
+            roomNumber=roomNumber,
+            cost=cost
         )
         Housekeeping_req.save()
     return render(request, 'hotel/create_request.html')
 
+
 def manage_housekeeping(request):
     req = Housekeeping_request.objects.all().order_by('request_date')
-    context = {'req' : req}
+    context = {'req': req}
     return render(request, 'hotel/manage_housekeeping.html', context)
+
 
 def hk_req_result(request):
     install_requests = list(Housekeeping_request.objects.all())
     objlist = []
     if request.method == "POST":
         for obj in install_requests:
-            if(str(request.POST.get('id')) == str(obj.id)):
-                objlist.append(obj)    
+            if (str(request.POST.get('id')) == str(obj.id)):
+                objlist.append(obj)
         context = {'req': objlist}
         return render(request, 'hotel/hk_req_result.html', context)
-    
+
+
 def update_result_hk(request):
     objlist = []
-    hk_requests = list(Housekeeping_request.objects.all())    
+    hk_requests = list(Housekeeping_request.objects.all())
     if request.method == "POST":
         for obj in hk_requests:
             print(str(request.POST['id']))
             print(str(obj.id))
-            if(str(request.POST.get('id')) == str(obj.id)):
+            if (str(request.POST.get('id')) == str(obj.id)):
                 obj.results = request.POST['description']
                 obj.cost = request.POST['cost']
                 objlist.append(obj)
                 obj.save()
         context = {'req': objlist}
     return render(request, 'hotel/hk_req_result.html', context)
+
 
 def service_food(request):
     if (get_room_number(request) == None):
@@ -225,7 +235,9 @@ def service_food(request):
 
     current = Order.objects.filter(email=email).filter(status=0)
     if (len(current) == 0):
-        mx = Order.objects.latest('id')
+        mx = 1
+        if (len(Order.objects.all) > 0):
+            mx = Order.objects.latest('id')
         current = Order(food_list="", price=0,
                         email=email, status=0, id=mx.id + 1)
         current.save()
